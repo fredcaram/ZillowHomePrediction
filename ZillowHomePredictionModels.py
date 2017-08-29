c35cf76fd682d90ec77350f49daf3e7a57bdac35
Frederico <fluiz@avenuecode.com>
2017-08-29 19:08:34 -0300

    Score: 0.0644429

--- ZillowHomePredictionModels.py
+++ ZillowHomePredictionModels.py
@@ -12,14 +12,26 @@ mingw_path = 'C:\\Program Files\\mingw-w64\\x86_64-7.1.0-posix-seh-rt_v5-rev2\\m
 os.environ['PATH'] = mingw_path + ';' + os.environ['PATH']
 import xgboost as xgb
 
+
+
 # Global Parameters
 XGB_WEIGHT = 0.6415
 BASELINE_WEIGHT = 0.0056
 OLS_WEIGHT = 0.0828
 
-XGB1_WEIGHT = 0.90738#0.8083  # Weight of first in combination of two XGB models
+XGB1_WEIGHT = 0.80#0.90738#0.8083  # Weight of first in combination of two XGB models
 
 BASELINE_PRED = 0.0115   # Baseline based on mean of training data, per Oleg
+
+xgb_params1_sklearn = {
+    'learning_rate': 0.0371,
+    'max_depth': 5,
+    'subsample': 0.81,
+    'booster': 'gblinear',
+    'reg_lambda': 0.8,
+    'reg_alpha': 0.4,
+    'silent': True
+}
 
 xgb_params1 = {
             'eta': 0.0371,
@@ -120,22 +132,44 @@ class ZillowHomePredictionModels:
         oof_test[:] = oof_test_skf.mean(axis=0)
         return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
 
+    def get_oof_for_xgboost(self, x_train, y_train, x_test, xgb_params, num_boost_rounds):
+        # Some useful parameters which will come in handy later on
+        ntrain = x_train.shape[0]
+        ntest = x_test.shape[0]
+        SEED = 42  # for reproducibility
+        NFOLDS = 5  # set folds for out-of-fold prediction
+        kf = KFold(n_splits=NFOLDS, random_state=SEED)
+        xgb_params['base_score'] = y_train.mean()
+
+        oof_train = np.zeros((ntrain,))
+        oof_test = np.zeros((ntest,))
+        oof_test_skf = np.empty((NFOLDS, ntest))
+        x_train = x_train.values
+        x_test = x_test.values
+
+        for i, (train_index, test_index) in enumerate(kf.split(x_train)):
+            x_tr = x_train[train_index]
+            y_tr = y_train[train_index]
+            x_te = x_train[test_index]
+
+            clf = xgb.train(xgb_params, xgb.DMatrix(x_tr, y_tr), num_boost_round=num_boost_rounds)
+
+            oof_train[test_index] = clf.predict(xgb.DMatrix(x_te))
+            oof_test_skf[i, :] = clf.predict(xgb.DMatrix(x_test))
+
+        oof_test[:] = oof_test_skf.mean(axis=0)
+        return oof_train.reshape(-1, 1), oof_test.reshape(-1, 1)
+
     def generate_combined_xgb_pred(self, x_train, y_train, x_test):
-        dtest = xgb.DMatrix(x_test)
-        xgb_model1 = self.generate_xgb_model(x_train, y_train, xgb_params1, 250)
-        xgb_train_pred1 = self.__get_model_prediction__(xgb_model1, xgb.DMatrix(x_train))
-        xgb_test_pred1 = self.__get_model_prediction__(xgb_model1, dtest)
-        #xgb1_oof_train, xgb1_oof_test = self.get_oof(xgb_model1, x_train, y_train, x_test)
+        #dtest = xgb.DMatrix(x_test)
+        xgb_train_pred1, xgb_test_pred1 = self.get_oof_for_xgboost(x_train, y_train, x_test, xgb_params1, 250)
 
-        xgb_model2 = self.generate_xgb_model(x_train, y_train, xgb_params2, 150)
-        xgb_train_pred2 = self.__get_model_prediction__(xgb_model2, xgb.DMatrix(x_train))
-        xgb_test_pred2 = self.__get_model_prediction__(xgb_model2, dtest)
-        #xgb2_oof_train, xgb2_oof_test = self.get_oof(xgb_model1, x_train, y_train, x_test)
+        xgb_train_pred2, xgb_test_pred2 = self.get_oof_for_xgboost(x_train, y_train, x_test, xgb_params2, 150)
 
-        new_x_train = pd.DataFrame({"model1": xgb_train_pred1, "model2": xgb_train_pred2})
-        new_x_test = pd.DataFrame({"model1": xgb_test_pred1, "model2": xgb_test_pred2})
+        new_x_train = pd.DataFrame({"model1": xgb_train_pred1.flatten(), "model2": xgb_train_pred2.flatten()})
+        new_x_test = pd.DataFrame({"model1": xgb_test_pred1.flatten(), "model2": xgb_test_pred2.flatten()})
         #combined_xgb_model = self.generate_xgb_model(new_x_train, y_train, xgb_params1, 150)
-        combined_xgb_pred = XGB1_WEIGHT*xgb_test_pred1 + (1-XGB1_WEIGHT)*xgb_test_pred2
+        combined_xgb_pred = XGB1_WEIGHT*xgb_test_pred1.flatten() + (1-XGB1_WEIGHT)*xgb_test_pred2.flatten()
         #new_d_test = xgb.DMatrix(new_x_test)
         #combined_xgb_pred = self.__get_model_prediction__(combined_xgb_model, new_d_test)
 
@@ -180,8 +214,6 @@ class ZillowHomePredictionModels:
         y_mean = y_train.mean()
         dtrain = xgb.DMatrix(X_train, y_train)
         xgb_params['base_score'] = y_mean
-
-        clf = xgb.XGBRegressor(xgb_params)
 
         # cv_result = xgb.cv(xgb_params,
         #                    dtrain,
